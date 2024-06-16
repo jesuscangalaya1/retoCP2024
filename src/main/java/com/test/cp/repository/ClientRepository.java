8c5d12f3fc0adcfbc1e96cd81fb9d622b899eb65
jesuscangalaya1 <jesuscangalaya1@gmail.com>
2024-06-16 18:18:44 -0500

    crud products

--- src/main/java/com/test/cp/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.cp.repository;
-
-import com.test.cp.entity.ClientEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<ClientEntity, Long>{
-}
